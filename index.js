ce2d46378b49cb70b879a5301463c3dcfa02ec92
ravi9879 <144209099+ravi9879@users.noreply.github.com>
2024-08-30 10:36:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,35 +1,35 @@
-const mconnect = require('./db')
+// const mconnect = require('./db')
 const express = require('express');
-const cp = require('cookie-parser');
-const cors = require('cors');
-const dy = require('body-parser');
-const app = express();
+// const cp = require('cookie-parser');
+// const cors = require('cors');
+// const dy = require('body-parser');
+// const app = express();
 
 
-mconnect();
+// mconnect();
 
 
-// {
-//     // origin: ["http://localhost:3000/"],
-//     methods: ["POST", "GET"],
-//     credentials: true
-// }
+// // {
+// //     // origin: ["http://localhost:3000/"],
+// //     methods: ["POST", "GET"],
+// //     credentials: true
+// // }
 
 
-app.use(cp());
-app.use(cors());
-app.use(dy.json());
-app.use(dy.urlencoded({ extended: true }));
-app.use('/', require('./router/payment'));
-// app.use('/', require('./router/mid'));
-// app.use('/', require('./router/user'));
-app.use('/', require('./router/add-cart'));
-app.use('/', require('./router/delete'));
-app.use('/', require('./router/login'));
-app.use('/', require('./router/sign_in'));
-app.use('/', require('./router/data'));
-app.use('/', require('./router/get-cart'));
-// app.use('/', require('./router/update'));
+// app.use(cp());
+// app.use(cors());
+// app.use(dy.json());
+// app.use(dy.urlencoded({ extended: true }));
+// app.use('/', require('./router/payment'));
+// // app.use('/', require('./router/mid'));
+// // app.use('/', require('./router/user'));
+// app.use('/', require('./router/add-cart'));
+// app.use('/', require('./router/delete'));
+// app.use('/', require('./router/login'));
+// app.use('/', require('./router/sign_in'));
+// app.use('/', require('./router/data'));
+// app.use('/', require('./router/get-cart'));
+// // app.use('/', require('./router/update'));
 
 app.get('/' ,(req,res)=>{
     res.json("hello") ;
